cc53be53ef310ae751b12575fff77b6fa08e2584
Victor Goecking <victorgoecking96@gmail.com>
2022-08-11 21:31:30 -0300

    style component Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Button } from "./components/Button"
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
